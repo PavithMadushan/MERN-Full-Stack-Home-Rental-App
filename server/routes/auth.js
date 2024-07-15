024ed39ca5ddbcbf9f405b5af7bbd7b79cfab9b4
PavithMadushan <pavithmadushan@gmail.com>
2024-07-15 11:40:47 +0530

    project added

---
+++ server/routes/auth.js
@@ -0,0 +1,98 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+
+const User = require("../models/User");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public//uploads/");
+  },
+
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+router.post("/register", upload.single("profileImage"), async (req, res) => {
+  try {
+    const { firstName, lastName, email, password } = req.body;
+
+    /*file is available at req.file*/
+    const profileImage = req.file;
+
+    if (!profileImage) {
+      return res.status(400).send("No file uploaded");
+    }
+
+    const profileImagePath = profileImage.path;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(409).json({ message: "User already exists" });
+    }
+
+    /*hash the password*/
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    /*create new user*/
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+      profileImagePath,
+    });
+
+    /*save the user*/
+
+    await newUser.save();
+
+    res
+      .status(200)
+      .json({ message: "User registered successfully!", user: newUser });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Registration failed!", user: message });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user=await User.findOne({email});
+
+    if(!user){
+        return res.status(400).json({message:"User doesn't exist"})
+    }
+
+    const isMatch=await bcrypt.compare(password,user.password);
+
+    if(!isMatch){
+        return res.status(400).json({message:"Invalid Credentials!"})
+    }
+
+    const token=jwt.sign({id:user._id},process.env.JWT_SECRET);
+
+    delete user.password;
+
+    res.status(200).json({token,user})
+
+
+
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({error:err.message })
+
+
+  }
+});
+
+module.exports = router;
